17ceb1c36c6fd9854bdaa3f57fd43aa1f9bc9b3b
league <league@imac-18.attlocal.net>
2025-06-20 16:55:33 -0700

    xgdvdbx

--- src/_01_Sorting_Algorithms/BogoSorter.java
+++ src/_01_Sorting_Algorithms/BogoSorter.java
@@ -3,23 +3,47 @@ package _01_Sorting_Algorithms;
 import java.util.Random;
 
 public class BogoSorter extends Sorter {
-    public BogoSorter() {
-        type = "Bogo";
-    }
+	public BogoSorter() {
+		type = "Bogo";
+	}
 
-    /*
-     * Bogo sort is a joke sorting algorithm. It is considered the most
-     * inefficient sorting algorithm while still maintaining the possibility
-     * of eventually sorting data.
-     * 
-     * It works by following these steps:
-     * STEP 1. Is the array in order?
-     * if yes, finished; if no, go to step 2.
-     * STEP 2. Take two random elements in the array and swap them.
-     * STEP 3. Go back to step 1.
-     */
-    @Override
-    void sort(int[] array, SortingVisualizer display) {
-        
-    }
-}
+	/*
+	 * Bogo sort is a joke sorting algorithm. It is considered the most inefficient
+	 * sorting algorithm while still maintaining the possibility of eventually
+	 * sorting data.
+	 * 
+	 * It works by following these steps: STEP 1. Is the array in order? if yes,
+	 * finished; if no, go to step 2. STEP 2. Take two random elements in the array
+	 * and swap them. STEP 3. Go back to step 1.
+	 */
+	Random q = new Random();
+	Boolean sorted = false;
+
+
+	@Override
+	void sort(int[] arr, SortingVisualizer display) {
+
+		while (sorted == false) { // while not sorted
+		
+					int e = q.nextInt(arr.length);
+					int u = q.nextInt(arr.length);
+					int p = arr[u];
+					arr[u] = arr[e];
+					arr[e] = p;
+				display.updateDisplay();
+			}
+			for (int i = 0; i < arr.length - 1; i++) {
+				if (arr[i] > arr[i + 1]) {
+					break;
+				}
+				if(i == arr.length - 2) {
+					sorted = true;
+				}
+				
+			}
+			
+			
+			
+		}
+	}
+
--- src/_01_Sorting_Algorithms/BubbleSorter.java
+++ src/_01_Sorting_Algorithms/BubbleSorter.java
@@ -4,14 +4,28 @@ public class BubbleSorter extends Sorter{
     public BubbleSorter() {
         type = "Bubble";
     }
-
+    int startIndex = 1;
     /* 
      * Use the bubble sorting algorithm to sort the array.
      * You can use display.updateDisplay() to show the current
      * progress on the graph.
      */
     @Override
-    void sort(int[] array, SortingVisualizer display) {
-        
+    void sort(int[] arr, SortingVisualizer display) {
+    	
+    	for(int r = 0; r < arr.length; r++) {
+		for (int i = startIndex; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) {
+              int t = arr[i];
+              arr[i] = arr[i - 1];
+              arr[i - 1] = t;
+
+              startIndex = i;
+             
+            }
+          } 
+		  display.updateDisplay();
+          startIndex = 1;
+    	}
     }
 }
