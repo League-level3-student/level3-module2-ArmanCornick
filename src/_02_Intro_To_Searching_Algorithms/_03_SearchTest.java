385407fc391ecb62ab79808b86682553e30da0a3
league <league@imac-18.attlocal.net>
2025-06-27 16:58:40 -0700

    nb

--- src/_02_Intro_To_Searching_Algorithms/_03_SearchTest.java
+++ src/_02_Intro_To_Searching_Algorithms/_03_SearchTest.java
@@ -12,14 +12,21 @@ class _03_SearchTest {
 
     @Test
     public void testLinearSearch() {
+    	String list1[] = {"cow", "sheep", "llama", "rooster", "pig", "chicken", "calf"};
         // 1. Use the assertEquals() method to test your linear search method
-        assertEquals(false, true);
+        assertEquals(5, _01_LinearSearch.linearSearch(list1, "Chicken".toLowerCase()));
+        assertEquals(3, _01_LinearSearch.linearSearch(list1, "rooster"));
+        assertEquals(-1, _01_LinearSearch.linearSearch(list1, "cat"));
     }
 
     @Test
     public void testBinarySearch() {
         // 2. Use the assertEquals() method to test your binary search method
         //    remember that the array must be sorted
-        assertEquals(false, true);
+    	int list2[] = {1,2,3,7,11,24,56,83,102};
+        // 1. Use the assertEquals() method to test your linear search method
+        assertEquals(5, _02_BinarySearch.binarySearch(list2, 0, list2.length-1, 24));
+        assertEquals(3, _02_BinarySearch.binarySearch(list2, 0, list2.length-1, 7));
+        assertEquals(-1, _02_BinarySearch.binarySearch(list2, 0, list2.length-1, 23));
     }
 }
--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -12,6 +12,61 @@ public class Algorithms {
     }
     
     public static int findBrokenEgg(List<String> eggs) {
-        return 0;
+        for(int i=0; i<eggs.size();i++) {
+        	if(eggs.get(i).equals("cracked")) {
+        		return i;
+        	}
+        }
+		return -1;
     }
-}
+    
+    public static int countPearls(List<Boolean> oysters) {
+    	int z=0;
+        for(int i=0; i<oysters.size();i++) {
+        	if(oysters.get(i)) {
+        		z += 1;
+        	}
+        }
+		return z;
+    }
+    
+    public static double findTallest(List<Double> peeps) {
+    	double z=0;
+        for(int i=0; i<peeps.size();i++) {
+        	if(peeps.get(i)>z) {
+        		z = peeps.get(i);
+        	}
+        }
+		return z;
+    }
+    
+    public static String findLongestWord(List<String> words) {
+    	String z= "";
+        for(int i=0; i<words.size();i++) {
+        	if(words.get(i).length()>z.length()) {
+        		z = words.get(i);
+        	}
+        }
+		return z;
+    }
+    
+    public static Boolean containsSOS(List<String> morse) {
+        for(int i=0; i<morse.size();i++) {
+        	if(morse.get(i).equals("... --- ...")) {
+        		return true;
+        	}
+        }
+		return false;
+    }
+    
+    public static List<Double> sortScores(List<Double> scores) {
+        for(int i=0; i<scores.size();i++) {
+        	for(int z=0; z<scores.size();z++) {
+            	if(scores.get(z)>scores.get(z-1)) {
+            		
+            	}
+            }
+        }
+		return null;
+    
+}}
